1e76331ca5915407e11f9a0a0d17cb25ac0dbbb8
ferchogmv15 <86312360+ferchogmv15@users.noreply.github.com>
2021-06-28 10:50:29 -0500

    ajustes agregar segundo boton y negrilla en los valores del escaner

--- app/src/main/java/com/MacPollo/lectorfacturas/ScannerActivity.java
+++ app/src/main/java/com/MacPollo/lectorfacturas/ScannerActivity.java
@@ -1,20 +1,15 @@
 package com.MacPollo.lectorfacturas;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.text.Html;
 import android.widget.TextView;
 
 import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
-import com.budiyev.android.codescanner.DecodeCallback;
-import com.google.zxing.Result;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -37,87 +32,72 @@ public class ScannerActivity extends AppCompatActivity {
         codeScanner = new CodeScanner(this, codeScannerView);
         codeScanner.setFormats(CodeScanner.ALL_FORMATS);
 
-        codeScanner.setDecodeCallback(new DecodeCallback() {
-            @Override
-            public void onDecoded(@NonNull Result result) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        // productivo
-                        // String url = "http://ap2021.macpollo.com/apiv1/api/factura/consultafactura";
-                        // pruebas
-                        String url = "http://ap2021.macpollo.com/apiprueba/api/factura/consultafactura";
-                        // desarrollo
-                        //String url = "http://192.168.1.7:8000/api/factura/consultafactura";
-                        String numero = result.getText();
-                        if (esValido(numero)) {
-                            HashMap data = new HashMap();
-                            data.put("factura", completar(numero));
-                            JSONObject parameters = new JSONObject(data);
+        codeScanner.setDecodeCallback(result -> runOnUiThread(() -> {
+            // productivo
+            // String url = "http://ap2021.macpollo.com/apiv1/api/factura/consultafactura";
+            // pruebas
+            String url = "http://ap2021.macpollo.com/apiprueba/api/factura/consultafactura";
+            // desarrollo
+            //String url = "http://192.168.1.7:8000/api/factura/consultafactura";
+            String numero = result.getText();
+            if (esValido(numero)) {
+                HashMap<String, String> data = new HashMap<>();
+                data.put("factura", completar(numero));
+                JSONObject parameters = new JSONObject(data);
 
-                            JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url,
-                                    parameters, new Response.Listener<JSONObject>() {
-                                @Override
-                                public void onResponse(JSONObject response) {
-                                    try{
-                                        if (response.toString().indexOf("message") == -1) {
-                                            // Get the JSON array
-                                            String tipo = response.getString("tipo");
-                                            if (tipo.equals("S")) {
-                                                JSONObject factura = response.getJSONObject("TFactura");
-                                                StringBuilder mensaje = new StringBuilder("La factura Nro. ");
-                                                mensaje.append(numero).append("\n por Valor de $");
-                                                String valor = format(String.valueOf(factura.getInt("Valor")));
-                                                mensaje.append(valor);
-                                                mensaje.append("\n a nombre de ");
-                                                mensaje.append(factura.getString("Name1")).append("\n");
-                                                String abono = format(String.valueOf(factura.getInt("Abono")));
-                                                mensaje.append("Valor pagado $").append(abono);
-                                                String saldo = format(String.valueOf(factura.getInt("Saldo")));
-                                                mensaje.append("\n Saldo $").append(saldo);
-                                                txt.setText(mensaje.toString());
-                                            } else {
-                                                txt.setText("Factura Nro. " + numero  +"\n");
-                                                String mensaje = response.getString("mensaje");
-                                                txt.append(mensaje);
-                                            }
-                                        } else {
-                                            txt.setText("Factura Nro. " + numero  +"\n");
-                                            String mensaje = response.getString("message");
-                                            txt.append(mensaje);
-                                        }
-
-                                    }catch (JSONException e){
-                                        e.printStackTrace();
+                JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url,
+                        parameters, response -> {
+                            try{
+                                if (!response.toString().contains("message")) {
+                                    // Get the JSON array
+                                    String tipo = response.getString("tipo");
+                                    if (tipo.equals("S")) {
+                                        JSONObject factura = response.getJSONObject("TFactura");
+                                        StringBuilder mensaje = new StringBuilder("La factura Nro. ");
+                                        mensaje.append("<b>").append(numero).append("</b>").append("<br> por Valor de $");
+                                        String valor = format(String.valueOf(factura.getInt("Valor")));
+                                        mensaje.append("<b>").append(valor).append("</b>");
+                                        mensaje.append("<br>a nombre de ");
+                                        mensaje.append("<b>").append(factura.getString("Name1")).append("</b><br>");
+                                        String abono = format(String.valueOf(factura.getInt("Abono")));
+                                        mensaje.append("Valor pagado $").append("<b>").append(abono).append("</b>");
+                                        String saldo = format(String.valueOf(factura.getInt("Saldo")));
+                                        mensaje.append("<br> Saldo $").append("<b>").append(saldo).append("</b>");
+                                        txt.setText(Html.fromHtml(mensaje.toString()));
+                                    } else {
+                                        txt.setText(Html.fromHtml("Factura Nro. <b>" + numero  +"</b><br>"));
+                                        String mensaje = response.getString("mensaje");
+                                        txt.append(mensaje);
                                     }
+                                } else {
+                                    txt.setText(Html.fromHtml("Factura Nro. <b>" + numero  +"</b><br>"));
+                                    String mensaje = response.getString("message");
+                                    txt.append(mensaje);
                                 }
-                            }, new Response.ErrorListener() {
-                                @Override
-                                public void onErrorResponse(VolleyError error) {
-                                    txt.setText("Factura Nro. " + numero  +"\n");
-                                    txt.append("Error al consultar la factura: " + error.getMessage());
-                                }
-                            });
 
-                            // Add a request (in this example, called stringRequest) to your RequestQueue.
-                            MySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonRequest);
-                        } else {
-                            txt.setText("Factura Nro. " + numero  +"\n");
-                            txt.append("Nro. de factura no válido");
-                        }
-                    }
-                });
+                            }catch (JSONException e){
+                                e.printStackTrace();
+                            }
+                        }, error -> {
+                            txt.setText(Html.fromHtml("Factura Nro. <b>" + numero  +"</b><br>"));
+                            txt.append("Error al consultar la factura: " + error.getMessage());
+                        });
+
+                // Add a request (in this example, called stringRequest) to your RequestQueue.
+                MySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonRequest);
+            } else {
+                txt.setText(Html.fromHtml("Factura Nro. <b>" + numero  +"</b><br>"));
+                txt.append("Nro. de factura no válido");
             }
-        });
+        }));
     }
 
     public static boolean esValido(String str) {
-        return str.matches("\\d+");  //match a number entero de 1 al 10 digitos
+        return str.matches("\\d{1,10}");  //match a number entero de 1 al 10 digitos
     }
 
     public static String format(String input){
         StringBuilder sb = new StringBuilder();
-        boolean sw = false;
         int i = input.length();
         while (i - 3 > 0) {
             sb.insert(0, input.substring(i-3, i));
