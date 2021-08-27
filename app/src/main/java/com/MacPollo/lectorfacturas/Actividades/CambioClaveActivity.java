d37a5b9cb884dddd6494194a1a1017cd4d92bd94
Edgar Fernando Vega Morales <ferchogmv15@gmail.com>
2021-08-26 23:25:20 -0500

    ajustes ativities
    se agregan mas activities

---
+++ app/src/main/java/com/MacPollo/lectorfacturas/Actividades/CambioClaveActivity.java
@@ -0,0 +1,16 @@
+package com.MacPollo.lectorfacturas.Actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.MacPollo.lectorfacturas.R;
+
+public class CambioClaveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cambio_clave);
+    }
+}
